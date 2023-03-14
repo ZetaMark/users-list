8c16c76f5493afdf870b281a2ffb92dbaa5dc76f
ZetaMark <scarfred777@gmail.com>
2023-03-14 03:56:18 +0200

    table content is moved to a separate php file

---
+++ users.php
@@ -0,0 +1,20 @@
+<tr>
+    <td class="align-middle">
+    <div
+        class="custom-control custom-control-inline custom-checkbox custom-control-nameless m-0 align-top">
+        <input type="checkbox" class="custom-control-input" id="item-1">
+        <label class="custom-control-label" for="item-1"></label>
+    </div>
+    </td>
+    <td class="text-nowrap align-middle">Adam Cotter</td>
+    <td class="text-nowrap align-middle"><span>Active</span></td>
+    <td class="text-center align-middle"><i class="fa fa-circle active-circle"></i></td>
+    <td class="text-center align-middle">
+    <div class="btn-group align-top">
+        <button class="btn btn-sm btn-outline-secondary badge" type="button" data-toggle="modal"
+        data-target="#user-form-modal">Edit</button>
+        <button class="btn btn-sm btn-outline-secondary badge" type="button"><i
+            class="fa fa-trash"></i></button>
+    </div>
+    </td>
+</tr>
