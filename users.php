09053769827acb90dfa85b0cca1b8ded699cb5e2
ZetaMark <scarfred777@gmail.com>
2023-03-16 09:40:35 +0200

    fix delete button

--- users.php
+++ users.php
@@ -34,9 +34,9 @@ if($result = $conn->query($sql)){
     <div class="btn-group align-top">
         <button data-id="<?php echo $row["id"] ?>" class="btn btn-sm btn-outline-secondary badge edit-user" type="button" data-toggle="modal"
         data-target="#user-form-modal">Edit</button>
-        <button  data-toggle="modal"
-        data-target="#confirm-delete-modal" class="btn btn-sm btn-outline-secondary badge" type="button"><i
-        data-id="<?php echo $row["id"] ?>" class="fa fa-trash call-modal-delete-user"></i></button>
+        <button  data-id="<?php echo $row["id"] ?>" data-toggle="modal"
+        data-target="#confirm-delete-modal" class="btn btn-sm btn-outline-secondary badge call-modal-delete-user" type="button"><i
+         class="fa fa-trash "></i></button>
     </div>
     </td>
 </tr>
